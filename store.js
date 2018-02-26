2e7bf04a6658ce661224161591b15f563dbe6fca
Ivan Yip <ivan_yip@msn.com>
2018-02-26 11:19:16 +0800

    bug fix

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from "redux-thunk";
+import { createLogger } from 'redux-logger';
 
-const middleware = applyMiddleware(thunk)
 
 function reducer(state = {
   form: {}
@@ -11,5 +11,7 @@ function reducer(state = {
   }
   return state;
 }
+
+const middleware = applyMiddleware(thunk, createLogger());
 
 export default createStore(reducer, middleware);
